30ed6ba1f69e29cdda4b1f6924a29d047d027870
klein <1243265252@qq.com>
2024-12-05 18:25:56 +0800

    把编译产生的文件剔除了

--- build/astra_camera/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/klein/camera_ros2/install/astra_camera_msgs/share/astra_camera_msgs/package.sh"
--- build/astra_camera_msgs/astra_camera_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/klein/camera_ros2/build/astra_camera_msgs/astra_camera_msgs__py
-# Build directory: /home/klein/camera_ros2/build/astra_camera_msgs/astra_camera_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
